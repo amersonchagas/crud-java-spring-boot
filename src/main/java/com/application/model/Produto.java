ef8624b6bc57f8fa78666235735aa657fd28eb38
Amerson Chagas <amerson.chagas@gmail.com>
2022-10-27 15:55:03 -0300

    editar e excluir

--- src/main/java/com/application/model/Produto.java
+++ src/main/java/com/application/model/Produto.java
@@ -1,10 +1,18 @@
 package com.application.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,12 +21,24 @@ import lombok.Setter;
 
 @Entity
 @Table(name="produto")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor   
+@Getter @Setter  @NoArgsConstructor @AllArgsConstructor   
 public class Produto {
-    @Id
+    @Id //javax.persistence.
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Nome é Obrigatório")
+    @Size(min=2, max=30, message = "Deve ter entre {min} e {max} caracteres")
+    @Column(name = "nome", nullable = false)
     private String nome;
+
+    @NotNull(message = "Preço é Obrigatório")
+    @Column(name = "preco", nullable = false)
     private double preco;
+
+    @NotNull(message = "Quantidade é Obrigatório")
+    @Min(value = 1, message = "Quantidade Minima Obrigatória: {value}")
+    @Column(name = "quantidade", nullable = false)
     private int quantidade;    
 }
